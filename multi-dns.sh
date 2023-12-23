206fb77ae271769c52bddf0dafc250490715dcb7
Prakhar225 <90061878+Prakhar225@users.noreply.github.com>
2023-12-23 16:32:25 +0530

    Update multi-dns.sh

--- multi-dns.sh
+++ multi-dns.sh
@@ -6,6 +6,6 @@ MAIN_IP=$(hostname -I | cut -d' ' -f1)
 
 while read p; do
   /usr/bin/avahi-publish -a "$p" -R "$MAIN_IP" &
-done <mdns-aliases
+done </root/mdns-aliases
 
 while true; do sleep 10000; done
