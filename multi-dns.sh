238d6ed0641800e0432df897b0e2b51369b0d6d0
Prakhar225 <90061878+Prakhar225@users.noreply.github.com>
2023-12-23 16:27:25 +0530

    Create multi-dns.sh

---
+++ multi-dns.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+MAIN_IP=$(hostname -I | cut -d' ' -f1)
+
+while read p; do
+  /usr/bin/avahi-publish -a "$p" -R "$MAIN_IP" &
+done <mdns-aliases
+
+while true; do sleep 10000; done
